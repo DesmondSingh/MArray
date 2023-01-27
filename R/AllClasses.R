ab84ed25ab1284044049c2b057635b063ac3c4ef
DesmondSingh <87040685+DesmondSingh@users.noreply.github.com>
2023-01-27 16:45:27 -0500

    updated classes definition

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -7,7 +7,7 @@
 #' @rdname MArray
 #' @return Object of clas "MArray"
 #' @export
-#' @importFrom methods setClass
+#' @importFrom methods setClass setValidity
 #'
 #' @examples
 #' ma <- MakeMArrayDataSet(n_samples = 16L, n_features = 12L, with_seed = 8675)
@@ -18,38 +18,34 @@ MArray <- setClass("MArray",
 
                    slots = c(marray = "matrix",
                              fmeta = "data.frame",
-                             pmeta = "data.frame"),
+                             pmeta = "data.frame"))
 
-                   prototype = list(marray=matrix(0, dimnames=list('gene A','sample 1')),
-                                    fmeta=data.frame('Yes', row.names = 'gene A'),
-                                    pmeta=data.frame('M', row.names = 'sample 1')),
 
-                   validity = function(object){
-                     msg <- NULL
-                     valid <- TRUE
-                     if (nrow(marray(object)) != nrow(fmeta(object))) {
-                       valid <- FALSE
-                       msg <- c(msg,
-                                "Number of data and feature meta-data rows must be identical.")
-                     }
-                     if (ncol(marray(object)) != nrow(pmeta(object))) {
-                       valid <- FALSE
-                       msg <- c(msg,
-                                "Number of data rows and sample meta-data columns must be identical.")
-                     }
-                     if (!identical(rownames(marray(object)), rownames(fmeta(object)))) {
-                       valid <- FALSE
-                       msg <- c(msg,
-                                "Data and feature meta-data row names must be identical.")
-                     }
-                     if (!identical(colnames(marray(object)), rownames(pmeta(object)))) {
-                       valid <- FALSE
-                       msg <- c(msg,
-                                "Data row names and sample meta-data columns names must be identical.")
-                     }
-                     if (valid) TRUE else msg
-                   }
+setValidity("MArray", function(object){
+  msg <- NULL
+  valid <- TRUE
+  if (nrow(marray(object)) != nrow(fmeta(object))) {
+    valid <- FALSE
+    msg <- c(msg,
+             "Number of data and feature meta-data rows must be identical.")
+  }
+  if (ncol(marray(object)) != nrow(pmeta(object))) {
+    valid <- FALSE
+    msg <- c(msg,
+             "Number of data rows and sample meta-data columns must be identical.")
+  }
+  if (!identical(rownames(marray(object)), rownames(fmeta(object)))) {
+    valid <- FALSE
+    msg <- c(msg,
+             "Data and feature meta-data row names must be identical.")
+  }
+  if (!identical(colnames(marray(object)), rownames(pmeta(object)))) {
+    valid <- FALSE
+    msg <- c(msg,
+             "Data row names and sample meta-data columns names must be identical.")
+  }
+  if (valid) TRUE else msg
 
-)
 
+})
 
