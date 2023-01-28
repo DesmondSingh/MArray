c2bc5bdf1c086730c27e3877176389bcd7f7c1b3
DesmondSingh <87040685+DesmondSingh@users.noreply.github.com>
2023-01-28 10:21:49 -0500

    stopifnot mod

--- R/AllMethods.R
+++ R/AllMethods.R
@@ -10,7 +10,7 @@
 #' marray(ma_obj)
 setMethod("marray", "MArray", function(object){
 
-  stopifnot("test error message"=class(object) == "MArray")
+  stopifnot(class(object) == "MArray")
 
   object@marray
   })
