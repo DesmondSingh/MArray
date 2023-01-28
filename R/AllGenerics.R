72b5673b032a808255a153cbf2e62b7e3d25ebb6
DesmondSingh <87040685+DesmondSingh@users.noreply.github.com>
2023-01-27 19:51:55 -0500

    added titles to documentation for generics

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,24 +1,30 @@
+#' @title Getter for marray slot of MArray object
 #' @rdname marray
 #' @export
 setGeneric("marray", function(object, ...) standardGeneric("marray"))
 
+#' @title Getter for pmeta slot of MArray object
 #' @rdname pmeta
 #' @export
 setGeneric("pmeta", function(object) standardGeneric("pmeta"))
 
+#' @title Getter for fmeta slot of MArray object
 #' @rdname fmeta
 #' @export
 setGeneric("fmeta", function(object) standardGeneric("fmeta"))
 
+#' @title Setter for marray slot of MArray object
 #' @rdname marray<-
 #' @export
 setGeneric("marray<-",
            function(object, value) standardGeneric("marray<-"))
 
+#' @title Setter for fmeta slot of MArray object
 #' @rdname fmeta<-
 #' @export
 setGeneric("fmeta<-", function(object, value) standardGeneric("fmeta<-"))
 
+#' @title Setter for pmeta slot of MArray object
 #' @rdname pmeta<-
 #' @export
 setGeneric("pmeta<-", function(object, value) standardGeneric("pmeta<-"))
